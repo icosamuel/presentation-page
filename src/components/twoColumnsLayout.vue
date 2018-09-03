cf9829522bd11331bd4cc85df884393489d5a063
Icosamuel <begs1307@gmail.com>
2018-09-03 16:03:14 -0400

    text-align rulez

--- src/components/twoColumnsLayout.vue
+++ src/components/twoColumnsLayout.vue
@@ -2,21 +2,30 @@
   <v-layout row justify-center>
     <v-flex xs2>
       <v-card>
-        <v-card-text>
+        <v-card-text class="A">
           <slot name="A"/>
         </v-card-text>
       </v-card>
     </v-flex>
 
-    <v-flex xs6>
+    <v-flex xs5>
       <v-card>
-        <v-card-text>
+        <v-card-text class="B">
           <slot name="B"/>
         </v-card-text>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<style scoped>
+.A{
+  text-align: right;
+}
+.B{
+  text-align: left;
+}
+</style>
 
 <script>
 export default {
